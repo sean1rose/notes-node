b1e3905b596d15e079f0929a811188f557f6929f
Sean Rose <sean1rose@gmail.com>
2017-01-22 00:53:42 -0800

    fill in add notes func

--- notes.js
+++ notes.js
@@ -1,5 +1,31 @@
+const fs = require('fs');
+
 var addNote = (title, body) => {
-  console.log('adding note - ', title, body);
+  var notes = [];
+  var note = {
+    title,
+    body
+  };
+
+  // so long as file exists...
+  try {
+    // string version, still need to parse
+    var notesString = fs.readFileSync('notes-data.json')
+    // replace notes 
+    notes = JSON.parse(notesString);
+  } catch (e) {
+    // if file does not exist
+
+  }
+
+  // array of dupes
+  var duplicateNotes = notes.filter((note) => note.title === title);
+
+  if (duplicateNotes.length === 0){
+    notes.push(note);
+    // createa file, w/ json'ified notes array
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+  }
 };
 
 var getAll = () => {
